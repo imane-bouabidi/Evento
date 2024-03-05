a90e7554bad399dc589bac3e7a0dfd0417f794f4
imane-bouabidi <imaneimane20201@gmail.com>
2024-03-05 16:48:17 +0100

    base de donnees

---
+++ database/migrations/2024_03_05_103422_creer_table_event.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('titre');
+            $table->string('description');
+            $table->timestamp('date');
+            $table->string('places');
+            $table->time('duree');
+            $table->string('lieu');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
